cc7e4c5e2e823584f1e487b91e8bdc3532949a94
bebossi <bernardobossi77@gmail.com>
2023-07-10 05:09:19 -0300

    creating axios config for reqs

---
+++ src/api.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+const apiURLs = {
+  development: "http://localhost:1214",
+};
+
+const api = axios.create({ baseURL: apiURLs["development"] });
+
+api.interceptors.request.use((config) => {
+  // const loggedInUserJSON = localStorage.getItem("loggedInUser");
+  const token = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("Bearer="))
+    ?.split("=")[1];
+
+  if (token) {
+    (config as AxiosRequestConfig).headers = {
+      ...config.headers,
+      Authorization: `Bearer ${token}`,
+    };
+  }
+
+  return config;
+});
+
+export { api };
